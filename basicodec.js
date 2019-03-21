24007d90b96982b53126e9432b29f3bd729520fa
Guillaume Charmetant <cguille.dev@gmail.com>
2019-03-21 10:21:41 +0100

    make JS firefox compliant
    Firefox does not seem to support named capture groups in regex

--- basicodec.js
+++ basicodec.js
@@ -15,13 +15,13 @@ const authHeaderInput = document.getElementById('decode-auth-header');
 const decodedUsernameElt = document.getElementById('decoded-username');
 const decodedPasswordElt = document.getElementById('decoded-password');
 
-const authHeaderRegex = /(?:Authorization: *)?(?:Basic +)?(?<credentials>.*)/i;
+const authHeaderRegex = /(?:Authorization: *)?(?:Basic +)?(.*)/i;
 
 function decode() {
     const authHeader = authHeaderInput.value;
-    const matching = authHeaderRegex.exec(authHeader);
+    const matches = authHeaderRegex.exec(authHeader);
 
-    if (!matching) {
+    if (!matches) {
         decodedUsernameElt.textContent = '';
         decodedPasswordElt.textContent = '';
 
@@ -29,7 +29,7 @@ function decode() {
     }
 
     try {
-        const parts = atob(matching.groups.credentials).split(':', 2);
+        const parts = atob(matches[1]).split(':', 2);
 
         decodedUsernameElt.textContent = parts[0];
         decodedPasswordElt.textContent = parts[1];
